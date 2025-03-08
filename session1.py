4a3fa104b9d7c75daecc04c6f73dc0f6807cd70c
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-03-08 18:47:56 +0330

    first commit

---
+++ session1.py
@@ -0,0 +1,32 @@
+# print("Hello")
+# print(1+2)
+# print(45* 56)
+
+# x = 12
+# y = 13
+
+# z = x + y
+
+# print("x + y", z)
+# print("x", x)
+# print("y", y)
+
+# print(type(x))
+
+# name = "ilia"
+# print(type(name))
+
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+
+# family = "farokhi"
+
+# print(name + " " + family)
+
+
+number1 = int(input("enter a number: "))
+number2 = int(input("enter a number: "))
+
+print(number1 + number2)
