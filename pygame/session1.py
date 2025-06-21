82c9e1b36cfd5a28d3a0a6309a0c5432eee9a33f
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-06-21 18:26:07 +0330

    blalal

--- pygame/session1.py
+++ pygame/session1.py
@@ -10,17 +10,31 @@ pygame.mixer.music.load("./assets/sounds/jungle.wav")
 pygame.mixer.music.set_volume(0.5)
 # pygame.mixer.music.play(-1)
 
-my_font = pygame.font.Font("./assets/fonts/joystix monospace.otf", 23)
-welcome_text = my_font.render("welcome", True, (255, 0,0))
-
+# my_font = pygame.font.Font("./assets/fonts/joystix monospace.otf", 23)
+# welcome_text = my_font.render("welcome", True, (255, 0,0))
+FPS = 60
+clock = pygame.time.Clock()
+square = pygame.Rect(100, 100, 50, 50)
+go_down, go_up, go_left, go_right = (False, False, False, False)
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.blit(welcome_text,  (100, 300))
-    pygame.draw.rect(screen, "green", (200, 200, 100, 300), 5, 100)
-    pygame.draw.rect(screen, "green", (400, 200, 100, 300))
-    pygame.draw.line(screen, "purple", (300, 300), (600, 600), 10)
-    pygame.draw.ellipse(screen, "yellow", (150, 400, 100, 100))
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                go_down = True
+            if event.key == pygame.K_UP:
+                pass
+            if event.key == pygame.K_LEFT:
+                pass
+            if event.key == pygame.K_RIGHT:
+                pass
+
+    if go_down:
+        square.y += 10
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen, (255, 255, 255), square)
     pygame.display.update()
+    clock.tick(FPS)
